3d34cfa11177217062cc8d1fb2374c7a7dff0ea0
Geordie Connell <geordie.connell@gmail.com>
2019-01-14 16:21:56 -0500

    decode attempt

--- drills.js
+++ drills.js
@@ -1,27 +1,51 @@
-"use strict"
+'use strict';
 
 function jediName (firstName, lastName) {
-    result = [];
-    for (let i = 0; i < 3; i++){
-        result.push(lastName[i])
-    }; 
+  result = [];
+  for (let i = 0; i < 3; i++){
+    result.push(lastName[i]);
+  } 
     for (let i = 0; i < 2; i++){
-        result.push(firstName[i])
-    };
+    result.push(firstName[i]);
+  }
     let final = result.join('');
-    return final;
-};
+  return final;
+}
 
 function beyond(num){
-    if (typeof num !== 'number' || num === undefined){
-        console.log('Please enter a number')
-    } else if (isFinite(num) === false){
-        console.log('and beyond!')
-    } else if (num > 0){
-        console.log('to infinity!')
-    } else if (num < 0){
-        console.log('to negative infinity!')
-    } else if (num === 0){
-        console.log('staying home')
+  if (typeof num !== 'number' || num === undefined){
+    console.log('Please enter a number');
+  } else if (isFinite(num) === false){
+    console.log('and beyond!');
+  } else if (num > 0){
+    console.log('to infinity!');
+  } else if (num < 0){
+    console.log('to negative infinity!');
+  } else if (num === 0){
+    console.log('staying home');
+  }
+}
+
+function decode(message) {
+  let letters = [];
+  let message_array = message.split(' ');
+  for (let i = 0; i < message_array.length; i++) {
+    if(message_array[i][0] === 'a'){
+      letters.push(message_array[i][1]);
     }
+    if (message_array[i][0] === 'b') {
+      letters.push(message_array[i][2]);
+    }
+    if (message_array[i][0] === 'c') {
+      letters.push(message_array[i][3]);
+    }
+    if (message_array[i][0] === 'd') {
+      letters.push(message_array[i][4]);
+    }
+    else{
+      letters.push(' ');
+    }
+  }
+  let result = letters.join('');
+  return result;
 }
